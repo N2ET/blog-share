02ed98272c5752a20d577f0747d8c0db5255d6de
N2ET <n2et@qq.com>
2020-12-26 15:42:37 +0800

    feat: 支持设置分类
    测试：
    1. 分类显示正常

--- share/WizShare.php
+++ share/WizShare.php
@@ -55,7 +55,7 @@ class WizShare extends Share {
 //            直接使用时间戳会导致异常，XmlRpc.php中接受到的格式化的数据类型是int
 //            'dateCreated' => $docData['created'] / 1000,
             'dateCreated' => date('Ymd H:i:s', $docData['created'] / 1000),
-            'categories' => []
+            'categories' => [$docData['category']]
         ];
 
         $formattedData = $this->execHandlers('formatSharePostData', $ret);
